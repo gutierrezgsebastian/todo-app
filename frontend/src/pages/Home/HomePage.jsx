90b9a1dfcea2b88f2034c210a81669c854c1c6da
gutierrezgsebastian <gutierrezgsebastian@outlook.com>
2022-08-22 18:21:22 -0500

    Connecting React with FastAPI

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -1,12 +1,27 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import "./HomePage.scss";
 import Header from "../../components/Header/Header";
 import NavBar from "../../components/NavBar/NavBar";
 import SideBar from "../../components/SideBar/SideBar";
 import Footer from "../../components/Footer/Footer";
+import ListPage from "../List/ListPage";
 
 const Home = () => {
+  const [tareasState, setState] = useState([]);
+  const url = "/v1/get_tareas/";
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        if (response.status === 200) {
+          let datos = response.json();
+          return datos;
+        }
+      })
+      .then((data) => setState(data));
+  }, []);
+
   return (
     <Fragment>
       <Header />
